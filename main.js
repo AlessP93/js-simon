6884e1f27fd782482809a9e5cc2c8751b069974e
AlessP93 <alespiscino@libero.it>
2022-05-12 15:28:58 +0200

    logica finita

--- main.js
+++ main.js
@@ -7,5 +7,38 @@
 // * Individuate gli elementi di cui avete bisogno per realizzare il programma.
 
 
-//1. Visualizzare in pagina 5 numeri casuali con un alert
-let randomNumber = Math.floor((Math.random() * 50) + 1);
+//1. Visualizzare in pagina 5 numeri casuali creati con un ciclo, mostrando un alert
+let randomNumber;
+for (let i = 0; i < 5; i++) {
+    alert(Math.floor(Math.random() * 50) + 1);
+}
+
+// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+setTimeout(function() {
+    for (let i = 0; i < 5; i++) {
+        let userNumber = Number(prompt("Ricorda e inserisci i numeri apparsi poco prima"));
+    }
+}, 3000);
+
+// Dopo il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+let control;
+if (randomNumber === userNumber){
+    alert("Hai indovinato il numero!")
+} else [
+    alert("Hai sbaglito numero riprova!")
+]
+
+
+
+
+
+
+// chiedo all utente quanti secondi mancano alla cottura della pasta
+// dopo il tempo indicato ese un alert con "è pronta la pasta alessandro"
+
+//1. chiedo all utente i secondi
+// const second = Number(prompt("quanti secondi mancano?"));
+
+// setTimeout(function(){
+//     alert("la pasta è pronta");
+// }, second * 1000); 
